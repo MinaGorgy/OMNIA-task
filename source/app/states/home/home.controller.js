82357f163c384ff5926348c70c27282fb2353f5c
Mina <minamorcosgorgy@outlook.com>
2018-08-29 01:26:34 +0200

    updating js

--- source/app/states/home/home.controller.js
+++ source/app/states/home/home.controller.js
@@ -5,7 +5,7 @@ angular
 
     .module('layout.home')
 
-    .controller('homeController', function ( $scope, $log, fromRoot,toastr,getTechCrunchArticles ) {
+    .controller('homeController', function ( $scope, $log, fromRoot,toastr,getArticles ) {
 
         var vm = $scope.vm = {
             sayNo: function ( state ) {
@@ -13,13 +13,13 @@ angular
             },
             articles:{},
         };
-        getTechCrunchArticles.getNewsArticles(function(response){
+        getArticles.getNewsArticles(function(response){
             $scope.articles = response.data.articles;   
           });
 
 
     })
-    .service('getTechCrunchArticles', function($http){
+    .service('getArticles', function($http){
         this.getNewsArticles = function(callback){
        $http.get('https://newsapi.org/v2/everything?q=Apple&from=2018-08-27&sortBy=popularity&apiKey=30a8a1f614244af6a3195b094f61a554')
           .then(callback);
