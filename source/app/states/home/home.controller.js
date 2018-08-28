f923c1b9b2444f7ed1561b036453bb1e68c90c5d
Mina <minamorcosgorgy@outlook.com>
2018-08-29 01:33:47 +0200

    code comments

--- source/app/states/home/home.controller.js
+++ source/app/states/home/home.controller.js
@@ -6,8 +6,9 @@ angular
     .module('layout.home')
 
     .controller('homeController', function ( $scope, $log, fromRoot,toastr,getArticles ) {
-
+        // inializing variable from the scope 
         var vm = $scope.vm = {
+            //error toastr
             sayNo: function ( state ) {
                 toastr.error('Sorry but this not implemented.');
             },
@@ -19,11 +20,12 @@ angular
 
 
     })
+
+    //calling the web service
     .service('getArticles', function($http){
         this.getNewsArticles = function(callback){
        $http.get('https://newsapi.org/v2/everything?q=Apple&from=2018-08-27&sortBy=popularity&apiKey=30a8a1f614244af6a3195b094f61a554')
           .then(callback);
-          //inside services
           
         };
       })
